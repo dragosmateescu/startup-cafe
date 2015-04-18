247ab4b74a0ccbfb4a61f2c2b80ce47d695d015e
Dragos Mateescu <dmateescu@tremend.ro>
2015-04-18 17:57:31 +0300

    dragos last changes commit

---
+++ article-preview.php
@@ -0,0 +1,137 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Startup Cafe</title>
+
+  <link rel="icon" type="image/png" href="img/startup_32.png">
+  <link rel="stylesheet" href="dist/css/style.css">
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,300italic,100italic,100,300,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<?php include('header.php') ?>
+<div class="main-layout-container main-homepage-container container-fluid">
+  <div class="homepage-top article-top">
+    <div class="homepage-left">
+      <div class="homepage-left-row">
+        <div class="left-content">
+          <div class="homepage-logo-container">
+            <div class="logo">
+              <img src="img/logo.png" />
+            </div>
+            <div class="partners">
+              <div class="partners-text">Proiect sprijinit de:</div>
+              <div class="partners-logos">
+                <img src="img/hotnews-logo.png" />
+                <img src="img/google-logo.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="homepage-center article-center-content">
+          <div class="center-content">
+            <div class="layout-article-container">
+              <div class="homepage-article">
+                <div class="article-content-row">
+                  <div class="article-image-container">
+                    <div class="article-image">
+                      <img class="img-responsive" src="img/article-dummy.png" />
+                    </div>
+                  </div>
+                  <div class="article-details-container">
+                    <div class="article-details">
+                      <div class="article-title">
+                        <h1>Aenean mollis malesuada felis, eu varius
+                          diam tempus ac. In sit amet nisi erat.</h1>
+                      </div>
+                      <div class="subarticle-details">
+                        <h5>de George Costinescu</h5><h5>Martie 20, 2015</h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="article-inside-text">
+                    <p>
+                      In contextul modificarilor propuse de Guvern pentru Codul Fiscal, Florin Jianu, presedintele Patronatului Tinerilor
+                      Intreprinzatori si fost ministru pentru IMM a declarat vineri, 20 februarie ca a transmis premierului propunerea ca
+                      intreprinzatorii debutanti sa fie scutiti timp de 2 ani de la plata impozitelor in anumite conditii. Ovidiu Nicolescu
+                      (Consiliul National pentru IMM) a adaugat ca masura din proiectul de Cod Fiscal care prevede impozit minim de
+                      1500 de lei pentru microintreprinderile cu zero angajati va duce la desfiintarea a minimum 40.000 de firme.
+                    </p>
+                    <p>
+                      Propunerea Patronatului Tinerilor Intreprinzatori ia in calcul o modificare, in paralel cu Codul Fiscal, a "legii
+                      antreprenorului debutant" astfel incat aceasta categorie de firme sa nu plateasca impozit timp de 2 ani daca
+                      angajeaza cel putin doua persoane.
+                    </p>
+                    <p>
+                      Florin Jianu a precizat ca odata cu eliminarea criteriului de varsta in Legea infiintarii de microintreprinderi de catre
+                      intreprinzatorii debutanti au fost create 7.000 de astfel de companii, in anul 2014 iar alte 1.000 numai in primele
+                      doua luni ale acestui an.
+                    </p>
+                    <p>
+                      Vezi aici ce prevede legea modificata a intreprinzatorilor debutanti
+                    </p>
+                    <p>
+                      Jianu a mai vorbit si despre viitoarea Agentie pentru antreprenoriat, considerand ca este o initiativa asteptata,
+                      proiectul aflandu-se inca in dezbaterea Guvernului. Despre infiintarea acestei Agentii EurActiv a scris in
+                      exclusivitate la inceputul acestei luni:
+                    </p>
+                    <p>
+                      EXCLUSIV Guvernul va infiinta o agentie pentru antreprenoriat care sa gestioneze fondurile destinate tinerilor,
+                      inclusiv "Garantia pentru tineret" a Uniunii Europene
+                    </p>
+                    <p>
+                      Ovidiu Nicolescu, presedintele Consiliului National pentru IMM a comentat cu privire la prevederea problematica
+                      din proiectul de Cod Fiscal care se refera la impozitele pentru microintreprinderi. In opinia sa, impunerea unei
+                      sume minime de 1530 de lei impozit pe venit va duce la desfiintarea a 40.000 - 50.000 de companii. "In conditiile
+                      in care este nevoie de cel putin cateva luni pentru a demara activitatea unei firma si a incepe sa ai incasari,
+                      aceasta conditie va determina pe multi intreprinzatori sa renunte, iar efectele vor fi negative pentru bugetul de stat
+                      fiindca ei vor deveni asistati social", a explicat Nicolescu.
+                    </p>
+                    <p>
+                      Propunerile legate de impozitarea microintreprinderilor, in proiectul de Cod Fiscal:
+                    </p>
+                    <p>
+                      Cote de impozitare diferentiate, in functie de numarul de salariati:
+                    </p>
+                    <p>
+                      1% pentru microintreprinderile cu 2 sau mai multi salariati
+                      3% pentru microintreprinderile cu un singur salariat
+                      3% + 1530 lei trimestrial, pentru microintreprinderile care nu au salariati
+                      "Impactul asupra bugetului va fi pozitiv de 316 mil. le"
+                    </p>
+                    <p>
+                      Citeste despre prevederile detaliate din proiectul de cod fiscal aici
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="homepage-right">
+      <div class="right-content">
+        <div class="adsense-container">
+          <div class="square-banner">
+            <img src="img/adsense-banner.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('footer.php') ?>
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="dist/js/bootstrap.js"></script>
+<script src="//localhost:35729/livereload.js"></script>
+</body>
+</html>
